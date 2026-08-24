07b0a5083c7c4cc18962ebfb4af64d84debe99fa
Ratnesh Mishra <ratneshmishra9131@gmail.com>
2026-08-25 04:31:28 +0530

    Time: 0 ms (100%), Space: 19.2 MB (90.99%) - LeetHub

--- 0868-binary-gap/0868-binary-gap.py
+++ 0868-binary-gap/0868-binary-gap.py
@@ -1,22 +1,13 @@
 class Solution:
     def binaryGap(self, n: int) -> int:
         ans = 0
-        binary = []
+        pos = 1
+        prev = -1
         while n > 0:
-            rem = n % 2
-            binary.append(rem)
+            if n % 2:
+                if prev != -1:
+                    ans = max(ans, pos - prev)
+                prev = pos
+            pos += 1
             n //= 2
-
-        i = 0
-        while i < len(binary):
-            if binary[i] == 1:
-                temp = i
-                i += 1
-                while i < len(binary) and binary[i] != 1:
-                    i += 1
-
-                if i < len(binary):
-                    ans = max(ans, i - temp)
-                    i -= 1
-            i += 1
         return ans
