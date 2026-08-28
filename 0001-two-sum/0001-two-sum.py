6c49599d37e415059c872b29c1137d59e822d049
Ratnesh Mishra <ratneshmishra9131@gmail.com>
2026-08-28 07:42:48 +0530

    Fix variable names in twoSum function

--- 0001-two-sum/0001-two-sum.py
+++ 0001-two-sum/0001-two-sum.py
@@ -1,8 +1,8 @@
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
-        Dict = {}
+        SecNum = {}
         for i in range(len(nums)):
             a = target-nums[i]
-            if a in Dict:
-                return [Dict[a], i]
-            Dict[nums[i]] = i
+            if a in SecNum:
+                return SecNum[a], i]
+            SecNum[nums[i]] = i
