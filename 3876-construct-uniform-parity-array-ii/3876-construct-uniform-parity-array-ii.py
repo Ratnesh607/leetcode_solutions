1400794452f49cf07160c58af6cd2b51fd05f29b
Ratnesh Mishra <ratneshmishra9131@gmail.com>
2026-09-04 04:52:00 +0530

    Time: 56 ms (62.72%), Space: 35.6 MB (17.88%) - LeetHub

---
+++ 3876-construct-uniform-parity-array-ii/3876-construct-uniform-parity-array-ii.py
@@ -0,0 +1,17 @@
+class Solution:
+    def uniformArray(self, nums1: List[int]) -> bool:
+
+        minOdd = float('inf')
+
+        for num in nums1:
+            if num % 2 == 1:
+                minOdd = min(minOdd, num)
+
+        if minOdd == float('inf'):
+            return True
+
+        for num in nums1:
+            if num % 2 == 0 and num < minOdd:
+                return False
+
+        return True
