0e91d20b6b1d330e63d24996dfba357b89273659
Ratnesh Mishra <ratneshmishra9131@gmail.com>
2026-09-19 20:21:16 +0530

    Refactor product calculation in maximum product function

--- 2233-maximum-product-after-k-increments/2233-maximum-product-after-k-increments.py
+++ 2233-maximum-product-after-k-increments/2233-maximum-product-after-k-increments.py
@@ -5,9 +5,9 @@ class Solution:
             heapq.heapreplace(nums, nums[0] + 1)
             k -= 1
 
-        product = 1
+        ans = 1
         MOD = 10**9 + 7
         for i in nums:
-            product *= i
-            product %= MOD
-        return product
+            ans *= i
+            ans %= MOD
+        return ans
