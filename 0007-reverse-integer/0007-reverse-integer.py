744f57e1ebb5a0216e65943482a9f2b1395db85b
Ratnesh Mishra <ratneshmishra9131@gmail.com>
2026-09-20 22:25:43 +0530

    Refactor reverse integer logic
    Refactor reverse function to simplify sign handling and variable names.

--- 0007-reverse-integer/0007-reverse-integer.py
+++ 0007-reverse-integer/0007-reverse-integer.py
@@ -1,16 +1,14 @@
 class Solution:
     def reverse(self, x: int) -> int:
         sign = 1
-        if x != abs(x):
+        if x < 0:
             sign = -1
-        temp = abs(x)
+        num = abs(x)
         ans = 0
-        while temp:
+        while num:
             ans *= 10
-            ans += temp % 10
-            temp //= 10
+            ans += num % 10
+            num //= 10
         ans *= sign
-        if ans < -2**31 or ans > 2**31 - 1:
-            return 0
-        return ans
+        return 0 if (ans < -2**31 or ans > 2**31 - 1) return ans
         
